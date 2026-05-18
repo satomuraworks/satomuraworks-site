7cd2d59fca13f5dc2ba9710f7545d2d575e89781
佐藤み。 <yomochan.1230@gmail.com>
2026-05-18 12:19:25 +0900

    Update works.ts

--- src/data/works.ts
+++ src/data/works.ts
@@ -112,7 +112,7 @@ export const works: Work[] = [
     category: 'fanart',
     tags: ['キャラクター', '衣装'],
     image: '/images/works/006riku.png',
-    description: '表情と衣装を確認できるキャラクターイラストです。',
+    description: 'キャラクターイラストです。',
     tools: 'Procreate',
     productionTime: '7時間',
     isFanart: true,
